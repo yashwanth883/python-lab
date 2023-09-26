6f9cfdfa745f78ca8b8694f8bf38b2caee4c657d
Yashwanth G R <109802642+yashwanth883@users.noreply.github.com>
2023-09-27 00:55:45 +0530

    Update program4_b.py

--- program4_b.py
+++ program4_b.py
@@ -14,4 +14,34 @@ def roman2dec(romStr):
     return value
 
 romanStr=input("Enter a Roman number:")
-print(roman2dec(romanStr))    
+print(roman2dec(romanStr))   
+
+
+
+
+
+def roman_to_integer(roman):
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    result = 0
+    prev_value = 0
+
+    for char in reversed(roman):
+        value = roman_dict[char]
+
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+
+        prev_value = value
+
+    return result
+
+roman_numeral = input("Enter a Roman numeral: ").upper()
+integer_value = roman_to_integer(roman_numeral)
+
+print(f"The integer value of {roman_numeral} is {integer_value}")
